a334d73a730517eee202117a8fc021b1392f7c21
Hepheir <hepheir@gmail.com>
2022-03-25 14:17:47 +0900

    feat(`Text`): create class `Text`

---
+++ src/main/java/racingcar/Text.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+public class Text {
+    public static final String BAR = "-";
+    public static final String WORD_SEPERATOR = ",";
+    public static final String INPUT_GUIDE_FOR_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
+    public static final String INPUT_GUIDE_FOR_TRIES = "시도할 회수는 몇회인가요?\n";
+    public static final String RESULT_HEADER = "실행 결과\n";
+    public static final String RESULT_OF_CAR = "%s : %s\n";
+    public static final String WINNER = "최종 우승자 : %s\n";
+    public static final String ERROR = "[ERROR] %s\n";
+}
