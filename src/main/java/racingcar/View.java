7475b5ebd133fd9902d600d00a400f40df4326c1
Hepheir <hepheir@gmail.com>
2022-03-26 10:10:42 +0900

    refactor(`Output`, `View`): extract output related methods out of `View`

--- src/main/java/racingcar/View.java
+++ src/main/java/racingcar/View.java
@@ -1,31 +1,28 @@
 package racingcar;
 
 import java.util.List;
-import java.util.function.Consumer;
 
 public class View {
-    private static final Consumer<Object> printFunction = x -> System.out.print(x);
-
     static void printLineBreak() {
-        printFunction.accept('\n');
+        Output.print('\n');
     }
 
     static void printInputGuideForCarName() {
-        printFunction.accept(Text.INPUT_GUIDE_FOR_CAR_NAMES);
+        Output.print(Text.INPUT_GUIDE_FOR_CAR_NAMES);
     };
 
     static void printInputGuideForTries() {
-        printFunction.accept(Text.INPUT_GUIDE_FOR_TRIES);
+        Output.print(Text.INPUT_GUIDE_FOR_TRIES);
     };
 
     static void printResultHeader() {
-        printFunction.accept(Text.RESULT_HEADER);
+        Output.print(Text.RESULT_HEADER);
     };
 
     static void printCar(Car car) {
         String name = car.getName();
         String position = repeat(Text.BAR, car.getPosition());
-        printFunction.accept(String.format(Text.RESULT_OF_CAR, name, position));
+        Output.print(String.format(Text.RESULT_OF_CAR, name, position));
     }
 
     private static String repeat(String source, int times) {
@@ -38,10 +35,10 @@ public class View {
 
     static void printWinners(List<String> namesOfWinners) {
         String joinedNames = String.join(Text.WORD_SEPERATOR, namesOfWinners);
-        printFunction.accept(String.format(Text.WINNER, joinedNames));
+        Output.print(String.format(Text.WINNER, joinedNames));
     };
 
     static void printError(String message) {
-        printFunction.accept(String.format(Text.ERROR, message));
+        Output.print(String.format(Text.ERROR, message));
     };
 }
